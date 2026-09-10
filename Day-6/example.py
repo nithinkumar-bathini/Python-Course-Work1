9f04adab1137d54ee22b02b8157f23a84f9dd852
nithinkumar-bathini <nithinkumarbathini.com>
2026-09-10 21:02:41 +0530

    String Operations

---
+++ Day-6/example.py
@@ -0,0 +1,34 @@
+# string
+username = "Nithin"
+#int
+age = 21
+#float
+waleet_balence = 500.00
+#bool
+location_permission = True
+#list
+ride_types = ["Bike", "Auto", "Cab"]
+#tuple
+pickup_location = (17.3850, 78.4867)
+#set
+available_payment_methods = {"UPI","Cash","Card"}
+#dictionary
+user = {
+    "name" : "Nithin",
+    "age" : 21,
+    "phone" : "8309761912",
+    "city" : "Hyderabad"
+}
+#nne
+promo_code = None
+
+# print all valuees
+print("USer Name:", user_name)
+print("Age:", age)
+print("Wallet Balance:", wallet_balance)
+print("Location Permission:", location_permission)
+print("Ride Types:", ride_types)
+print("Pickup Location:", pickup_location)
+print("Payment Methods:", available_payment_methods)
+print("User Details:", user)
+print("Promo Code:", promo_code)
