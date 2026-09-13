5d17a7dc22483969675be768331022e30fffed98
Nithin Kumar Bathini <nithinkumarbathini@gmail.com>
2026-09-13 12:35:18 +0530

    Delete Day-10 directory

--- Day-10/ifelse.py
+++
@@ -1,9 +0,0 @@
-'''  username = input("Username: ")
-password = input("Password: ")
-if username == "nithin" and password == "nithin123":
-    print("Login Successful")
-else:
-    print("Invalid Cradentials")   '''
-
-
-
