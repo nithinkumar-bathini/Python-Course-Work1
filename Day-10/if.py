14aba68ee12518368a066cff18feeef699f35097
nithinkumar-bathini <nithinkumarbathini.com>
2026-09-13 20:57:08 +0530

    Conditional Statements

---
+++ Day-10/if.py
@@ -0,0 +1,17 @@
+'''
+sales = int(input("Enter the sales: "))
+
+if sales > 1000:
+    print("Best Seller")      '''
+
+'''
+eli_acc = eval(input("Eligible Accout: "))
+var_sub = eval(input("Meta Verified Subsription: "))
+
+if eli_acc and var_sub:
+    print("Verified Badge Granted")    '''
+
+'''  rain_status = eval(input("Enter the rain Status: "))
+if rain_status:
+    print("Extra Rain Charge Applied")  '''
+
---
+++ Day-10/ifelse.py
@@ -0,0 +1,9 @@
+'''  username = input("Username: ")
+password = input("Password: ")
+if username == "nithin" and password == "nithin123":
+    print("Login Successful")
+else:
+    print("Invalid Cradentials")   '''
+
+
+
